075cd58a90fe6aa5ba942ed2305fdbddfeaff7ac
neildhaddha01 <65908724+neildhaddha01@users.noreply.github.com>
2021-04-27 12:59:19 +0530

    Add files via upload

---
+++ SettingsScreen.js
@@ -0,0 +1,23 @@
+import React,{Component}from 'react';
+import {
+    View,
+    Text,
+    TextInput,
+    Modal,
+    KeyboardAvoidingView,
+    StyleSheet,
+    TouchableOpacity,
+    Alert,
+    ScrollView,
+    FlatList} from 'react-native';
+
+
+export default class SettingsScreen extends Component{
+    render(){
+        return(
+            <View style={{flex: 1}}>
+                <Text>Settings Screen</Text>
+            </View>
+        )
+    }
+}
